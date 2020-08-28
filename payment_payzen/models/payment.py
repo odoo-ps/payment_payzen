17927e36650576e5c52c8f398e37ec9b35af2536
LOP <pierrelocus@gmail.com>
2020-08-28 05:06:01 +0200

    [TYPO] mispaste

--- payment_payzen/models/payment.py
+++ payment_payzen/models/payment.py
@@ -242,7 +242,7 @@ class AcquirerPayzen(models.Model):
         so = self.env['sale.order'].search([('name', '=', reference)])
         if so and so.payment_acquier_id:
             if self.provider == 'payzenmulti':
-                tx_values = self._alter_with_so_data_alter_with_so_data(tx_values, so, amount)
+                tx_values = self._alter_with_so_data(tx_values, so, amount)
             if so.partner_id.customer_nbr:
                 tx_values.update({'vads_cust_id': str(so.partner_id.customer_nbr)})
 
