070c8c831b55da8b58779ad2f6c4df02381e1276
LOP <pierrelocus@gmail.com>
2020-09-04 11:40:19 +0200

    [FIX] Computing values before mail

--- payment_payzen/models/payment.py
+++ payment_payzen/models/payment.py
@@ -282,13 +282,9 @@ class AcquirerPayzen(models.Model):
 
     def _alter_with_so_data(self, tx_values, so, amount):
         has_first_payment = True
-        so.first_payment_amount_mail = so.first_payment_amount
         if not so.first_payment_amount:
             has_first_payment = False
-            so.first_payment_amount_mail = amount / int((self.payzen_multi_count or 1)) / 100
-        first, monthly, last = self._get_payments_so(so, amount)
-        so.payzen_payment_monthly_amount = monthly
-        so.monthly_payment = monthly
+
         first_date = so.date_order.split(' ')[0]
         sec_date = datetime.strptime(first_date, '%Y-%m-%d')
         sec_date = so.second_payment_date or (sec_date + relativedelta(days=+(int(self.payzen_multi_period) or 30))).strftime('%Y-%m-%d')
@@ -307,52 +303,42 @@ class AcquirerPayzen(models.Model):
             capture_delay = abs((first_date.date() - today).days)
             sub_effect_date = datetime.strptime('-'.join(sec_date), '%Y-%m-%d')
 
-            if first + (monthly * int(int(self.payzen_multi_count) - 1)) != amount:
-                first_sub_amount = (amount - first) - (monthly * int(int(self.payzen_multi_count) - 2))
-                so.first_sub_payment_amount_mail = first_sub_amount / 100
+            if so.first_sub_payment_amount_mail:
                 tx_values.update({
                     'vads_sub_init_amount_number': u'1',
-                    'vads_sub_init_amount': str(first_sub_amount)
+                    'vads_sub_init_amount': str(so.first_sub_payment_amount_mail)
                 })
             tx_values.update({
                 'vads_sub_desc': vads_sub_desc,
                 'vads_page_action': u'REGISTER_PAY_SUBSCRIBE',
-                'vads_amount': str(first),
+                'vads_amount': str(so.first_payment_amount_mail),
                 'vads_payment_config': u'SINGLE',
-                'vads_sub_amount': str(monthly),
+                'vads_sub_amount': str(so.payzen_payment_monthly_amount),
                 'vads_sub_effect_date': str(sub_effect_date.strftime('%Y%m%d')),
                 'vads_capture_delay': str(capture_delay)
             })
         else:
             vads_sub_desc += u'COUNT=' + str(self.payzen_multi_count) + u';'
-            if monthly * int(self.payzen_multi_count) != amount:
-                first_sub_amount = amount - (monthly * int(int(self.payzen_multi_count) - 1))
-                so.first_sub_payment_amount_mail = first_sub_amount
+            if so.first_sub_payment_amount_mail:
                 tx_values.update({
-                    'vads_amount': str(first_sub_amount),
+                    'vads_amount': str(so.first_sub_payment_amount_mail),
                     'vads_sub_init_amount_number': u'1',
-                    'vads_sub_init_amount': str(first_sub_amount)
+                    'vads_sub_init_amount': str(so.first_sub_payment_amount_mail)
                 })
             else:
                 tx_values.update({
-                    'vads_amount': str(monthly)
+                    'vads_amount': str(so.payzen_payment_monthly_amount)
                 })
             tx_values.update({
                 'vads_sub_desc': vads_sub_desc,
                 'vads_page_action': u'REGISTER_SUBSCRIBE',
-                'vads_sub_amount': str(monthly),
+                'vads_sub_amount': str(so.payzen_payment_monthly_amount),
                 'vads_sub_effect_date': u''.join([str(i) for i in first_date.split('-')])
             })
         
         _logger.info('tx_values : ')
         _logger.info(tx_values)
         return tx_values
-
-    def _get_payments_so(self, so, amount):
-        first = int(so.first_payment_amount_mail * 100)
-        monthly = int(round((amount - first) / (int(self.payzen_multi_count) - 1)))
-        last = int(amount - first - ((int(self.payzen_multi_count) - 2) * monthly))
-        return first, monthly, last
 
 class TransactionPayzen(models.Model):
     _inherit = 'payment.transaction'
--- payment_payzen/models/sale_order.py
+++ payment_payzen/models/sale_order.py
@@ -9,9 +9,9 @@ class SaleOrder(models.Model):
     second_payment_date = fields.Date('Second payment date')
     first_payment_amount = fields.Monetary('First amount to pay')
     payzen_get_months = fields.Integer(compute='_on_payment_acquier_id')
-    first_payment_amount_mail = fields.Monetary()
-    first_sub_payment_amount_mail = fields.Monetary()
-    payzen_payment_monthly_amount = fields.Monetary()
+    first_payment_amount_mail = fields.Monetary(compute='_get_payzen_amounts')
+    first_sub_payment_amount_mail = fields.Monetary(compute='_get_payzen_amounts')
+    payzen_payment_monthly_amount = fields.Monetary(compute='_get_payzen_amounts')
 
     @api.onchange('order_line')
     def _onchange_order_line(self):
@@ -29,3 +29,27 @@ class SaleOrder(models.Model):
         for r in self:
             if r.payment_acquier_id:
                 r.payzen_get_months = r.payment_acquier_id.payzen_multi_count
+
+    @api.depends('payment_acquier_id', 'amount_total')
+    def _get_payzen_amounts(self):
+        for sale in self:
+            if sale.first_payment_amount:
+                sale.first_payment_amount_mail = sale.first_payment_amount
+            first, monthly = sale._get_payzen_amounts()
+            amount = sale.amount_total * 100
+            sale.payzen_payment_monthly_amount = monthly
+            if first + (monthly * int(int(sale.payment_acquier_id.payzen_multi_count) - 1)) != amount:
+                first_sub_amount = (amount - first) - (monthly * int(int(sale.payment_acquier_id.payzen_multi_count) - 2))
+                sale.first_sub_payment_amount_mail = first_sub_amount / 100
+
+
+    @api.onchange('first_payment_amount')
+    def _do_recompute(self):
+        self.ensure_one()
+        self.recompute()
+
+    def _get_payments_so(self):
+        self.ensure_one()
+        first = int(self.first_payment_amount_mail * 100)
+        monthly = int(round((self.amount_total * 100 - first) / (int(self.payment_acquier_id.payzen_multi_count) - 1)))
+        return first, monthly
