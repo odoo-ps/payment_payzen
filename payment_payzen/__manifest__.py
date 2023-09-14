fd8c67df4293b79a520fb08dc3fd974cdcadc5fb
abk16 <abt@odoo.com>
2023-09-14 15:46:15 +0200

    [ADD] payment_payzen: 16.0.3.0.3 version from lyra/plugin-odoo release

---
+++ payment_payzen/__manifest__.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+#
+# Copyright © Lyra Network.
+# This file is part of PayZen plugin for Odoo. See COPYING.md for license details.
+#
+# Author:    Lyra Network (https://www.lyra.com)
+# Copyright: Copyright © Lyra Network
+# License:   http://www.gnu.org/licenses/agpl.html GNU Affero General Public License (AGPL v3)
+
+{
+    'name': 'PayZen Payment Provider',
+    'version': '3.0.3',
+    'summary': 'Accept payments with PayZen secure payment gateway.',
+    'category': 'Accounting/Payment Providers',
+    'author': 'Lyra Network',
+    'website': 'https://www.lyra.com/',
+    'license': 'AGPL-3',
+    'depends': ['payment'],
+    'data': [
+        'views/payment_provider_views.xml',
+        'views/payment_payzen_templates.xml',
+        'data/payment_icon_data.xml',
+        'data/payment_provider_data.xml',
+        'security/ir.model.access.csv',
+    ],
+    'images': ['static/description/icon.png'],
+    'application': True,
+    'installable': True
+}
